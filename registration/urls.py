3eadec5845b606367eef2fad68bf97cfa7f1582e
Mark Steve Samson <hello@marksteve.com>
2015-06-14 22:30:29 +0800

    Add register page

--- registration/urls.py
+++ registration/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import include, url
 
+from . import views
+
 urlpatterns = [
-    url('^', include('django.contrib.auth.urls'))
+    url('^', include('django.contrib.auth.urls')),
+    url('^register', views.register, name='register'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('landing')
+    else:
+        form = UserCreationForm()
+    context = dict(form=form)
+    return render(request, "registration/register.html", context)
