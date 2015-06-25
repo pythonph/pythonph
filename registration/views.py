4176da43f18a7db288e69af4d03c5d15c5b8819e
Mark Steve Samson <hello@marksteve.com>
2015-06-25 10:43:50 +0800

    Add name and email info to register form

--- registration/views.py
+++ registration/views.py
@@ -1,9 +1,22 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
+
+
+class UserCreationForm(UserCreationForm):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "email", "username"]
 
 
 def register(request):
     if request.method == 'POST':
+        print request.POST
         form = UserCreationForm(data=request.POST)
         if form.is_valid():
             form.save()
