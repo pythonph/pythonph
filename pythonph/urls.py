544b0800cf1529353552156b74d50f7699c11da3
pandabearcoder <vlouie00@gmail.com>
2020-04-11 14:37:09 +0800

    Update project url config

--- pythonph/urls.py
+++ pythonph/urls.py
@@ -1,17 +1,12 @@
-import jobs
-import landing
-import registration
-import slack
-from django.conf.urls import include, patterns, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns(
-    '',
-    url(r'', include(landing.urls)),
-    url(r'', include(registration.urls)),
-    url(r'', include(slack.urls)),
-    url(r'^jobs/', include(jobs.urls)),
-    url(r'^jobs/api/', include(jobs.api.v1.urls)),
+urlpatterns = (
+    url(r'', include('landing.urls', namespace='landing')),
+    url(r'', include('registration.urls', namespace='registration')),
+    url(r'', include('slack.urls', namespace='slack')),
+    url(r'^jobs/', include('jobs.urls', namespace='jobs')),
+    url(r'^jobs/api/', include('jobs.api.v1.urls', namespace='jobs_api')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^markdown/', include('django_markdown.urls')),
 )
