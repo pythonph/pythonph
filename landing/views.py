b2f1131cab0a897b7db779867030c13479cbfdbf
pandabearcoder <vlouie00@gmail.com>
2021-05-22 19:09:26 +0800

    Retrieve officers list from the records

--- landing/views.py
+++ landing/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 
 from .models import Event, Section
+from organisation.models import Volunteer
 
 
 def index(request):
     events = Event.objects.all()
     sections = Section.objects.all()
+    board_members = Volunteer.objects.filter(is_staff=True)
     return render(request, 'landing/index.html', {
         'events': events,
-        'sections': sections
+        'sections': sections,
+        'board_members': board_members,
     })
