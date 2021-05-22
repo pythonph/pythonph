ad8954cd689859b97d1695cfc7c6b850d34ea67f
pandabearcoder <vlouie00@gmail.com>
2021-05-22 19:26:58 +0800

    Retrieve volunteer list from the records

--- landing/views.py
+++ landing/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 
 from .models import Event, Section
-from organisation.models import Volunteer
+from organisation.models import Volunteer, Commitee
 
 
 def index(request):
-    events = Event.objects.all()
-    sections = Section.objects.all()
-    board_members = Volunteer.objects.filter(is_staff=True)
+    events = Event.available_objects.all()
+    sections = Section.available_objects.all()
+    board_members = Volunteer.available_objects.filter(is_staff=True)
+    commitees = Commitee.available_objects.prefetch_related('volunteers')
+
     return render(request, 'landing/index.html', {
         'events': events,
         'sections': sections,
         'board_members': board_members,
+        'commitees': commitees,
     })
